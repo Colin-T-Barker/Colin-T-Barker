6592b99ec64a0b6a1760249fe0a5b1699db78e2c
Colin T Barker <cbarker01@drury.edu>
2026-09-08 15:59:47 -0500

    Learning more about types and whoa, p\to q works both as implies or as a function!

--- Learning Lean4/TestProject/Chapter 3 Practice/Chapter 3 Practice.lean
+++ Learning Lean4/TestProject/Chapter 3 Practice/Chapter 3 Practice.lean
@@ -1,2 +1,36 @@
 import «Chapter 3 Practice».Basic
 /- This is a project for me to go through the texts' third chapter-/
+/- Below is the copy from the text to show this program runs-/
+section first
+def Implies (p q : Prop) : Prop := p → q
+#check And
+#check Or
+#check Not
+#check Implies
+
+variable (p q r : Prop)
+
+#check And p q
+#check Or (And p q) r
+#check Implies (And p q) (And q p)
+end first
+
+structure Proof (p : Prop) : Type where
+  proof : p
+
+#check Proof
+
+axiom and_commut (p q : Prop) : Proof (Implies (And p q) (And q p))
+
+variable (p q : Prop)
+
+#check and_commut p q
+
+axiom modus_ponens (p q : Prop) : -- The idea is that given p and q, the proof of 'implies pq' implies that a proof of p implies a proof of q
+  Proof (Implies p q) → Proof p →
+  Proof q
+
+axiom implies_intro (p q : Prop) :
+  (Proof p → Proof q) → Proof (Implies p q)
+
+#check implies_intro -- BTW, "a proof of something" is *also* a type.
