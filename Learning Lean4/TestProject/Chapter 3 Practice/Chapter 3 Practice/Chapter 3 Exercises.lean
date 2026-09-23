59b52f4bec9a5d0a0e9e630ef76ad68e8402120a
Colin T Barker <cbarker01@drury.edu>
2026-09-23 15:29:39 -0500

    Got exercise 1 done, I saw a version on stackexchange and will see if I can replicate it or my version for future problems.

--- Learning Lean4/TestProject/Chapter 3 Practice/Chapter 3 Practice/Chapter 3 Exercises.lean
+++ Learning Lean4/TestProject/Chapter 3 Practice/Chapter 3 Practice/Chapter 3 Exercises.lean
@@ -3,9 +3,21 @@ variable (p q r : Prop)
 
 -- commutativity of ∧ and ∨
 example : p ∧ q ↔ q ∧ p :=
-    have hp: p := p ∧ q-- Ugh, so I'm not clear yet, but I think once I figure out a few of these I'll be happier.
-    have hq: q := p ∧ q
-    show p ∧ q ↔ q ∧ p from ⟨hp, hq⟩
+    Iff.intro -- this is telling LEAN to use an iff proof, LEAN expects both directions from functions
+        (fun h: p ∧ q =>
+            have hp : p := h.left
+            have hq : q := h.right
+            show q ∧ p from And.intro hq hp
+        ) -- this closes the LHS of iff
+        (
+            fun h: q ∧ p =>
+                have hq : q := h.left
+                have hp : p := h.right
+                show p ∧ q from And.intro hp hq
+        ) -- this closes the RHS of iff
+/- So the basic thing to learn here is that the way to think through this is to
+    Tell the interpreter what to expect in terms of format, then describe the proof
+    in terms of functions, etc.  I still need better info about what the ''.intro's are.-/
 
 
 example : p ∨ q ↔ q ∨ p := sorry
